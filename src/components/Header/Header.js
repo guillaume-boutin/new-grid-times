8f61ff26140c1c777e4babf85cdf865ccb4c1342
guillaume-boutin <guillaumeboutin2101@gmail.com>
2021-08-08 23:31:57 -0400

    My solution for Header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,10 +17,12 @@ const Header = () => {
             <button>
               <Search size={24} />
             </button>
+
             <button>
               <Menu size={24} />
             </button>
           </ActionGroup>
+
           <ActionGroup>
             <button>
               <User size={24} />
@@ -28,8 +30,25 @@ const Header = () => {
           </ActionGroup>
         </Row>
       </SuperHeader>
+
       <MainHeader>
+        <DesktopActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+
+          <button>
+            <Menu size={24} />
+          </button>
+        </DesktopActionGroup>
+
         <Logo />
+
+        <SubscribeWrapper>
+          <Button>Subscribe</Button>
+
+          <SignInLink href="">Already a subscriber?</SignInLink>
+        </SubscribeWrapper>
       </MainHeader>
     </header>
   );
@@ -39,6 +58,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+  
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +82,55 @@ const ActionGroup = styled.div`
   }
 `;
 
+const DesktopActionGroup = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    flex: 1;
+    margin-top: 12px;
+  }
+`;
+
+const SubscribeWrapper = styled.div`
+  display: none;
+  justify-self: end;
+  
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+    
+    & button {
+      margin-top: 32px;
+    }
+  }
+`;
+
+const SignInLink = styled.a`
+  font-family: var(--font-family-serif);
+  font-size: ${14 / 16}rem;
+  font-weight: var(--font-weight-normal);
+  font-style: italic;
+  text-decoration: underline;
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+  
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+  
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    margin-top: 16px;
+    margin-bottom: 72px;
+  }
 `;
 
 export default Header;
